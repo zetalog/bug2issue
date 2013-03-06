2ccb4238e449895b1d92fa42d53942722e5b8a26
Lv Zheng <lv.zheng@intel.com>
2013-03-06 09:19:47 +0800

    json: Export printbuf APIs for future use
    The "printbuf" API is useful for bug2issue to generate formatted json object
    content. This patch exports such APIs for future usage.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>

--- lib/json/json_private.h
+++ lib/json/json_private.h
@@ -77,43 +77,6 @@
 # define open _open
 #endif
 
-#undef PRINTBUF_DEBUG
-
-struct printbuf {
-  char *buf;
-  int bpos;
-  int size;
-};
-
-extern struct printbuf*
-printbuf_new(void);
-
-/* As an optimization, printbuf_memappend is defined as a macro that
- * handles copying data if the buffer is large enough; otherwise it
- * invokes printbuf_memappend_real() which performs the heavy lifting
- * of realloc()ing the buffer and copying data.
- */
-extern int
-printbuf_memappend(struct printbuf *p, const char *buf, int size);
-
-#define printbuf_memappend_fast(p, bufptr, bufsize)          \
-do {                                                         \
-  if ((p->size - p->bpos) > bufsize) {                       \
-    memcpy(p->buf + p->bpos, (bufptr), bufsize);             \
-    p->bpos += bufsize;                                      \
-    p->buf[p->bpos]= '\0';                                   \
-  } else {  printbuf_memappend(p, (bufptr), bufsize); }      \
-} while (0)
-
-extern int
-sprintbuf(struct printbuf *p, const char *msg, ...);
-
-extern void
-printbuf_reset(struct printbuf *p);
-
-extern void
-printbuf_free(struct printbuf *p);
-
 #define ARRAY_LIST_DEFAULT_SIZE 32
 
 typedef void (array_list_free_fn) (void *data);
